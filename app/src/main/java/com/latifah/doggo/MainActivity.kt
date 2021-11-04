f54294916671e2e383f46a052c6a826cac38488d
latifahpresident <latifah.pres@gmail.com>
2021-11-04 12:47:35 -0500

    created basic file structure

--- app/src/main/java/com/latifah/doggo/MainActivity.kt
+++ app/src/main/java/com/latifah/doggo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.latifah.doggo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//START BY CREATING A NAV GRAPH
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
