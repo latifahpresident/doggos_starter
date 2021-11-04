2c253589fc6411672f28ba3d02c2ef42aaecb000
latifahpresident <latifah.pres@gmail.com>
2021-11-04 16:48:54 -0500

    added fragments and nav graph

--- app/src/main/java/com/latifah/doggo/MainActivity.kt
+++ app/src/main/java/com/latifah/doggo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.latifah.doggo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.latifah.doggo.databinding.ActivityMainBinding
+
 
 //START BY CREATING A NAV GRAPH
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment_container))
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
